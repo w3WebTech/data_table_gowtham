2e5648646a2c9ed57afa5960d40966867803cc04
w3WebTech <164163622+w3WebTech@users.noreply.github.com>
2024-07-29 13:29:22 +0530

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ import svgLoader from 'vite-svg-loader'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    buildDir: 'dist',
   nitro: {
     preset: 'node-server'
   },
